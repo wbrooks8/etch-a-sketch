b596e19c4406424fce0de56e1ed83d0cb10fdb9f
Kyle Brooks <kyle.brooks1989@gmail.com>
2021-03-13 09:05:20 -0600

    added a reset button

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function grid () {
     let b = document.getElementById("count").value;
     let c = b * b;
 
-        for (i=0; i < c; i++) {
+            for (i=0; i < c; i++) {
 
             var block = document.createElement('div');
             block.className = "block";
@@ -13,11 +13,32 @@ function grid () {
         
             let grid = document.getElementById('container');
             grid.setAttribute('style','grid-template-columns: repeat(' + b + ', auto)');
-        }
+            }
+
 }
 
 function color (block){
-    block.setAttribute('class', "permahover")
+    block.setAttribute('class', "permahover");
 }
 
+var input = document.getElementById("count");
 
+input.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+   event.preventDefault();
+   document.getElementById("submitC").click();
+  }
+});
+
+function resetContainer() {
+
+    var elements = document.getElementsByClassName("block");
+    while (elements.length > 0) {
+      elements[0].parentNode.removeChild(elements[0]);
+    }
+
+    var elements2 = document.getElementsByClassName("permahover");
+    while (elements2.length > 0) {
+      elements2[0].parentNode.removeChild(elements2[0]);
+    }
+  }
